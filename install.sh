c59501754c7935df12396379f42ba2a2730a28ac
kartikmakkar94 <kartik.makkar1994@gmail.com>
2017-03-18 00:36:26 +0530

    Bash Script to install dependencies

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo pip install pandas
+sudo pip3 install pandas
+sudo sudo apt-get install libssl-dev
+sudo pip3 install quandl
+sudo pip install quandl
+sudo pip3 install sklearn
+sudo pip3 install scipy
+sudo pip install scipy
+sudo pip3 install matplotlib
+sudo pip install matplotlib
+sudo apt-get install python3-tk
+sudo apt-get install python-tk
